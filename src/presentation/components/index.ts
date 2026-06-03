6f8b84878b3d6896a3c241718c9ad11357f0de81
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-06-03 17:50:37 -0600

    feat: add GptOrthographyMessage component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,5 +1,6 @@
 export * from './chat-bubbles/GptMessage.tsx'
 export * from './chat-bubbles/MyMessage.tsx'
+export * from './chat-bubbles/GptOrthographyMessage.tsx'
 
 export * from './chat-input-boxes/TextMessageBox.tsx'
 export * from './chat-input-boxes/TextMessageBoxFile.tsx'
